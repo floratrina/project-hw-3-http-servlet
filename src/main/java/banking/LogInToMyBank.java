27d5b035c872c11a58beb4f780700aa37d7d45ce
mrahman <rahmanww@gmail.com>
2020-08-22 22:07:59 -0400

    http-servlet-hw-3

--- src/main/java/banking/LogInToMyBank.java
+++ src/main/java/banking/LogInToMyBank.java
@@ -5,6 +5,7 @@ package banking;
   Please implement servlet life cycle so that user from the browser can enter account number and pin number.
   After receiving the request, please parse the the user data and send the response to the client.
 
+  After implementing servlet,web.xml and index.jsp, deploy on Tomcat Server
  */
 
 public class LogInToMyBank {

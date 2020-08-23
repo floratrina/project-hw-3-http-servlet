485849319da177ab8c1b4c42878a013280d2922f
mrahman <rahmanww@gmail.com>
2020-08-22 22:05:54 -0400

    http-servlet-hw-3

---
+++ src/main/java/banking/LogInToMyBank.java
@@ -0,0 +1,22 @@
+package banking;
+
+
+/*
+  Please implement servlet life cycle so that user from the browser can enter account number and pin number.
+  After receiving the request, please parse the the user data and send the response to the client.
+
+ */
+
+public class LogInToMyBank {
+
+    //implement initialize method
+
+
+    //implement doGet method
+
+
+    //implement doPost method
+
+
+
+}
